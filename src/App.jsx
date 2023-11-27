2020fe5df6b26fb8b0909e4fa0a36d3bac9f6691
Sanila Chowdhury <sanilachowdhury@gmail.com>
2023-11-27 09:36:57 -0500

    added more starter code

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,29 @@ function App() {
             </p>
         </div>
     </div>
+
+
+    <div class="box-main">
+                    <div class="secondHalf">
+                        <h1 class="text-big" id="program">
+                            JavaScript Tutorial
+                        </h1>
+                        <p class="text-small">
+                            JavaScript is the world most
+                            popular lightweight, interpreted
+                            compiled programming language.
+                            It is also known as scripting
+                            language for web pages. It is
+                            well-known for the development
+                            of web page many non-browser
+                            environments also use it.
+                            JavaScript can be used for
+                            Client-side developments as well
+                            as Server-side developments.
+                        </p>
+                    </div>
+                </div>
+                
 </section>
 
   );
