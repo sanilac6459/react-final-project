ff6076b7f2a4e8fb748024eef3683646120cb6ac
Sanila Chowdhury <sanilachowdhury@gmail.com>
2023-11-27 09:32:26 -0500

    added starter code

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,20 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <h1>Hello World</h1>
-  )
+    <section class="section">
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+                Learning C++
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+</section>
+
+  );
 }
 
 export default App
