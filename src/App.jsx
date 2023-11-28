08df3d0070b1e8c7fba6f00400ed7a94d7b4e3a3
Sanila Chowdhury <sanilachowdhury@gmail.com>
2023-11-28 09:08:41 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,18 @@ function App() {
     <div class="box-main">
         <div class="firstHalf">
             <h1 class="text-big">
-                Learning C++
+                C++ info website
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn when programming in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            How to print statement and take in user’s input
             </h1>
             <p class="text-small">
                 Here is all the things you need to learn in C++
@@ -19,28 +30,160 @@ function App() {
         </div>
     </div>
 
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Return 0 in main function to end program
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
 
     <div class="box-main">
-                    <div class="secondHalf">
-                        <h1 class="text-big" id="program">
-                            JavaScript Tutorial
-                        </h1>
-                        <p class="text-small">
-                            JavaScript is the world most
-                            popular lightweight, interpreted
-                            compiled programming language.
-                            It is also known as scripting
-                            language for web pages. It is
-                            well-known for the development
-                            of web page many non-browser
-                            environments also use it.
-                            JavaScript can be used for
-                            Client-side developments as well
-                            as Server-side developments.
-                        </p>
-                    </div>
-                </div>
-                
+        <div class="firstHalf">
+            <h1 class="text-big">
+            New line
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Reading files from input
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Conditionals
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Loops
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Functions
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Strings and methods 
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Generate random numbers 
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            1D and 2D arrays 
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Pointers 
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Classes, constructors, methods 
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Vectors 
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Inheritance  
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+
+    <div class="box-main">
+        <div class="firstHalf">
+            <h1 class="text-big">
+            Recursion 
+            </h1>
+            <p class="text-small">
+                Here is all the things you need to learn in C++
+            </p>
+        </div>
+    </div>
+             
 </section>
 
   );
