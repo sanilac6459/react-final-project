15b7012cc0b35a5a5441f4a9b3990d4ea614eaf1
Sanila Chowdhury <sanilachowdhury@gmail.com>
2023-12-04 12:47:23 -0500

    added bg color in app.css

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
                     <li>At the end of every line [with some exceptions], remember to add a semi-colon or your program will break. The semicolon is kind of like the period in regular writing. It lets the compiler know when each line is done when it's reading the code.</li>
                     <li>When coding in the main function, <strong>int main()</strong>, add the syntax <strong>return 0; </strong> which will end your program once it's runned</li><br></br>
                     
-                     <h3> <li><strong>Print statements</strong></li> </h3>
+                     <h2> <li><strong>Print statements</strong></li> </h2>
                       <img src={helloWorld} alt="anime girl typing furiously to cout hello world" class = "center"/>
                         <ul>
                           <li>Use <strong>cout</strong> and the symbol <strong>{"<<"}</strong> to add onto your statement if needed</li>
@@ -53,16 +53,16 @@ function App() {
                             </ul>
                           
                         </ul><br></br>
-                        <h3> <li><strong>Taking in user's input</strong></li> </h3>
+                        <h2> <li><strong>Taking in user's input</strong></li> </h2>
                         <ul>
                           <li>Use <strong>cin</strong> and the symbol <strong>{">>"}</strong></li>
                         </ul><br></br>
-                        <h3>  <li><strong>New line</strong></li> </h3>
+                        <h2>  <li><strong>New line</strong></li> </h2>
                     <ul>
                       <li>Use <strong>\n</strong> or <strong>endl</strong> to add a new line after a statement
                       <br></br>For example, "\n [your string]" or "your string" {"<<"} endl; Both can be used, and not just with strings. Concatenating \n with ints, doubles, booleans, and more, is possible.</li>
                     </ul><br></br>
-                    <h3> <li><strong>String methods</strong></li> </h3>
+                    <h2> <li><strong>String methods</strong></li> </h2>
                     <ul>
                       <li><strong>.length()</strong> or <strong>.size()</strong> - finding the length of a string</li>
                       <ul>
@@ -70,9 +70,9 @@ function App() {
                       <li>cout {"<<"} <strong>txt.length();</strong> // outputs 12, including the spaces</li>
                       </ul>
                     </ul><br></br>
-                    <h3> <li><strong>Conditionals</strong></li> </h3>
+                    <h2> <li><strong>Conditionals</strong></li> </h2>
                     
-                    <h3> <li><strong>Loops</strong></li> </h3> 
+                    <h2> <li><strong>Loops</strong></li> </h2> 
                     <img src={fruits} alt="fruit loops overwhelm ppl trying to learn loops lol" class = "center"/>
                     <h3> <li><strong>Functions</strong></li> </h3>
                     <h3> <li><strong>Generating random numbers</strong></li> </h3>
