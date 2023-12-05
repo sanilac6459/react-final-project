374f4499b654a2de49795b41229a034a9b330482
Rachel Briskman <rachelbriskman@gmail.com>
2023-12-04 22:08:32 -0500

    Added gifs and described classes (OOP)

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import helloWorld from './img/coding-hello-world.gif'
 import emoji from'./img/emoji2.gif'
 import fruits from './img/fruit loops.gif'
 import bgMusic from "./assets/Hall-of-Horoscope.mp3"
+import ham from "./img/ham.gif"
+import recur from "./img/recursion.gif"
+import life from "./img/life.gif"
+import classExample from "./img/classDef.png"
+
 // BIG THINGS HAPPENING
 
 
@@ -112,7 +117,14 @@ function App() {
               Object oriented
               </h1>
               <p class="text-small">
-                  Here is all the things you need to learn in C++
+                  Object Oriented Programming (OOP) is based on the idea of classes. Classes are like templates, they have their own instance variables, functions, and are often used to store information in an orderly way. For example, we could make a person class. Each person has a name, age, birthdate, country of origin, etc. These values can be stored as strings, ints, doubles, you name it! Instead of declaring and initialzing each variable for every single person, the Person class can take care of that for you, sort of like a container for all that info. It's like if you asked a person to fill out an official government form with set questions, vs giving them a blank piece of paper and asking them to provide info about themselves. You wouldn't have to tell them "write your name, age, etc.", it would be written on the paper itself.
+                  <br></br> So, in simpler terms, classes can allow you to make custom variable types. Strings have their own set functions, as do ints, and doubles, chars, bools.. they are just data types. Classes are a way of making your own data types.
+                  <br></br><img src={classExample} alt="w3schools explain classes" class = "center"/><br></br>
+                  This image from w3schools shows a standard class declaration. Here, since the 2 variables are public, they may be accessed outside of the class. However, if the variables were private, you might have to make get and set member functions. The private keyword is a great way to restrict access to functions or variables outside of the class declaration, especially if you don't want people changing stuff willy nilly.
+                  Also, another commmon convention of classes is to create a constructor. Here, there is no constructor, but since all the variables are public, to set their value you can just do myObject.varName = value;
+                  Constructors are typically written as classname() under public: in the class declaration. Inside the constructor, it initalizes all the member variables to their default value. With a constructor, you can then make objects of that class by writing className objectName = new className(), assuming you are using the default constructor, which has no parameters. Nondefault constructors function the same way, except they use parameters as the values for the data members.
+                  <br></br><br></br> Now, let's imagine you actually have a life, (a life object made with a life class), below may be some functions that are part of the life class.
+                  <img src={life} alt="if code described life" class = "center"/>
               </p>
           </div>
       </div>
@@ -147,10 +159,14 @@ function App() {
               <p class="text-small">
                   Recursive functions are functions that call themselves, usually until a certain result is reached.
               </p>
-              <img src={emoji} alt="congrats emoji gif" class = "center"/>
+              <img src={recur} alt="recursive dancing" class = "center"/>
           </div>
       </div>
-  
+      <div class = "note">
+        <h2>Congratulations! You now know C++. Good job buddy, now let's learn CSS!! <br></br>(it's even more fun I swear)</h2>
+        <li><h3>Me: </h3></li><
+          img src={ham} alt="sad and flat" class = "center"/>
+      </div>
   </section>
   
     );
