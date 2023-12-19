e1e125be40df1333312645b4a4ebc3ae7ea0135a
maida5 <145588313+maida5@users.noreply.github.com>
2023-12-19 12:08:05 -0500

    Subclass explanation

--- src/App.jsx
+++ src/App.jsx
@@ -345,9 +345,30 @@ function App() {
               <u>Inheritance</u>  
               </h1>
               <p class="text-small">
-                  Before we learned that the purpose of <strong>Classes</strong> is to simplify our code and reduce redundancy. But what if you already have a great class, but need to add a few more things?
-                  <br></br> Sure, you can modify your existing class declaration, but that would be annoying, since you'd have to modify all your existing objects of that class as well.
+                  Before we learned that the purpose of <strong>Classes</strong> is to simplify our code and reduce redundancy. But what if you want to have some special cases in your class that not all the objects will share?
+                  <br></br> Sure, you can modify your existing class declaration, but that would be annoying, since not all of the cases apply to your intent of the object 
+                  <br></br> For example, let's say you have a Person class. Each person has a different occupation and each occupation has their own specific little details. A Person with a doctor occupation is going to have patients but a Person with a student occupation has no need for that. How can we solve this problem?
                   <br></br><strong>Solution: </strong> Create a <strong>subclass</strong>
+                  <ul>
+                    <li>A <strong>subclass</strong> derives all the public properties of an existing class while including it's own properties</li>
+                    <li>Similarly, a <strong>superclass</strong> is a class where it's properties are being inherited by a <strong>subclass</strong></li>
+                    <li>For example, let's say you have a Person class and the Student subclass</li>
+                    <ul>
+                      <li>The Person class would have properties such as name, age, birthday, etc.</li>
+                      <li>By making Student a subclass of Person and deriving the public properties of Person, Student would already have the properties name, age, birthday, etc. because it derived it from the Person class</li>
+                      <li>Therefore, you only need to make things that are specific to the Student class, such as GPA, classes, schedule, etc.</li>
+                      <ul>
+                        <li>This also shows another reason why you would need a Student subclass, since not every person object has a GPA or takes classes, but every Student object has a name and age just like every Person object</li>
+                      </ul>
+                    </ul>
+                    <li>Something of importance to remember is that the subclass will not have <strong>access</strong> to any private data members, but it can still inherit any private data members</li>
+                    
+                    <h2><li><strong><u>Using Subclasses</u></strong></li></h2>
+                    <ul>
+                      <li></li>
+                    </ul>
+
+                  </ul>
 
               </p>
               <img src={inheritance} alt="" class = "center"/>
