7b0e90afaf3c35a1aa218396dce9e1ce2456c853
maida5 <145588313+maida5@users.noreply.github.com>
2023-12-19 15:06:20 -0500

    Updated music

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 import logo from './img/logo.gif'
-import bgMusic from "./assets/Hall-of-Horoscope.mp3"
 import ham from "./img/ham.gif"
-import useSound from 'use-sound'
 import Basics from './components/Basics'
 import Pointers from './components/Pointers'
 import ObjectOriented from './components/ObjectOriented'
 import Vectors from './components/Vectors'
 import Inheritance from './components/Inheritance'
 import Recursion from './components/Recursion'
+import PlaySound from './components/Sound'
 
 // BIG THINGS HAPPENING
 
 
 function App() {
     const [count, setCount] = useState(0)
-    // function play()
-    // {
-    //   new Audio(bgMusic).play()
-    // }
-    const [playSound] = useSound(bgMusic);
     return (
       
   <section class="section">
-  <audio src="./assets/Hall-of-Horoscope.mp3" autoPlay loop />
+  <PlaySound/>
       <div class="box-main">
           <div class="firstHalf">
               <strong><h1 class="text-big">
@@ -33,11 +27,10 @@ function App() {
               <img src={logo} alt="C++ Users nerd emoji gif" class = "center"/>
               <h3 class="sub-heading">
                   Here is all the things you need to learn when programming in C++<br></br>
-                  (Yes, we woke up and chose violence :D )
+                  (Yes, we woke up and chose violence :D ) <br></br>
+                  <br></br>
+                  (Music by Soichi Terada)
               </h3>
-              <div class="sound-button">
-                <button onClick={playSound}>Play BGM (by Soichi Terada)</button>
-              </div>
           </div>
       </div>
           
